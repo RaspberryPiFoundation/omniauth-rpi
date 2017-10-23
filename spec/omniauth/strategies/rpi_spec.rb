618b97902da94f73e1acd11cada96ded60adf737
Greg Woodcock <greg.woodcock@raspberrypi.org>
2017-10-23 11:17:51 +0100

    Add tests for overriding default auth urls

--- spec/omniauth/strategies/rpi_spec.rb
+++ spec/omniauth/strategies/rpi_spec.rb
@@ -5,6 +5,21 @@ RSpec.describe OmniAuth::Strategies::Rpi do
   let(:parsed_response) { instance_double('ParsedResponse') }
   let(:response) { instance_double('Response', :parsed => parsed_response) }
 
+  let(:development_site)          { 'http://localhost:9000/api/v3' }
+  let(:development_authorize_url) { 'http://localhost:9000/login/oauth/authorize' }
+  let(:development_token_url)     { 'http://localhost:9000/login/oauth/access_token' }
+
+  let(:development) do
+    OmniAuth::Strategies::Rpi.new('RASPBERRY_KEY', 'RASPBERRY_SECRET',
+      {
+      :client_options => {
+        :site => development_site,
+        :authorize_url => development_authorize_url,
+        :token_url => development_token_url
+      }
+    })
+    end
+
   subject do
     OmniAuth::Strategies::Rpi.new({})
   end
@@ -24,6 +39,20 @@ RSpec.describe OmniAuth::Strategies::Rpi do
 
     it 'should have correct token url' do
       expect(subject.options.client_options.token_url).to eq('https://auth.raspberrypi.org/oauth2/token')
+    end
+
+    describe 'should be overrideable' do
+      it 'for site' do
+        expect(development.options.client_options.site).to eq(development_site)
+      end
+
+      it 'for authorize url' do
+        expect(development.options.client_options.authorize_url).to eq(development_authorize_url)
+      end
+
+      it 'for token url' do
+        expect(development.options.client_options.token_url).to eq(development_token_url)
+      end
     end
   end
 
