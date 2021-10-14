8e8beca8e93daeda0c0e23fbe4526f1c51c9b9f3
Greg Annandale <greg@raspberrypi.org>
2021-10-14 10:30:42 -0700

    Remove hydra1 strategy requirement

--- lib/omniauth-rpi.rb
+++ lib/omniauth-rpi.rb
@@ -1,5 +1,4 @@
 require 'omniauth-rpi/version' # rubocop:disable Naming/FileName
 require 'omniauth/strategies/hydra0'
-require 'omniauth/strategies/hydra1'
 require 'omniauth/strategies/rpi'
 require 'rpi_auth_bypass'
